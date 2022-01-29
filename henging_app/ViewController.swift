cee6db1a5e5be542c33b89a40ce312e5c0e18b7a
Tin Jurković <tjurkovic@iolap.com>
2022-01-29 19:13:03 +0100

    Initial Commit

---
+++ henging_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  henging_app
+//
+//  Created by Tin Jurkovic on 29.01.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ henging_appUITests/henging_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  henging_appUITestsLaunchTests.swift
+//  henging_appUITests
+//
+//  Created by Tin Jurkovic on 29.01.2022..
+//
+
+import XCTest
+
+class henging_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
